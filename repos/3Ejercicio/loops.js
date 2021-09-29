57569914a543929d8062bfd0054d81562dd3efd7
Raul115 <rulomiguelez@gmail.com>
2021-09-29 10:15:40 +0200

    Added new lines

--- repos/3Ejercicio/loops.js
+++ repos/3Ejercicio/loops.js
@@ -23,6 +23,14 @@ for (let i = 0; i < 5; ++i) alert(i);
 //porque hace primero la comparacion i < 5
 
 
+//Use the for loop to output even numbers from 2 to 10
+for(let i = 2; i < 10; i++){
+    if(i % 2 == 2){
+        alert(i)
+    }
+}
+
+
 for (let i = 0; i < 3; i++) {
     alert(`number ${i}!`);
 }
--- repos/4Ejercicio/switch.js
+++ repos/4Ejercicio/switch.js
@@ -1,26 +1,53 @@
-// switch (browser) {
-//     case 'Edge':
-//         alert("You've got the Edge!");
-//         break;
+// // switch (browser) {
+// //     case 'Edge':
+// //         alert("You've got the Edge!");
+// //         break;
 
-//     case 'Chrome':
-//     case 'Firefox':
-//     case 'Safari':
-//     case 'Opera':
-//         alert('Okay we support these browsers too');
-//         break;
+// //     case 'Chrome':
+// //     case 'Firefox':
+// //     case 'Safari':
+// //     case 'Opera':
+// //         alert('Okay we support these browsers too');
+// //         break;
 
-//     default:
-//         alert('We hope that this page looks ok!');
+// //     default:
+// //         alert('We hope that this page looks ok!');
+// // }
+
+// let browser = prompt("Type a browser")
+// if (browser == "edge") {
+//     alert("You've got the Edge!")
+// }
+// else if (browser == "chrome" || browser == "firefox" || browser == "safari" || browser == "opera") {
+//     alert("Okay we support these browsers too")
+// }
+// else {
+//     alert("We hope that this page looks ok!")
 // }
 
-let browser = prompt("Type a browser")
-if (browser == "edge") {
-    alert("You've got the Edge!");
-}
-else if (browser == "chrome" || browser == "firefox" || browser == "safari" || browser == "opera") {
-    alert('Okay we support these browsers too');
-}
-else {
-    alert('We hope that this page looks ok!');
+
+// // let a = +prompt('a?', '');
+
+// // if (a == 0) {
+// //   alert( 0 );
+// // }
+// // if (a == 1) {
+// //   alert( 1 );
+// // }
+
+// // if (a == 2 || a == 3) {
+// //   alert( '2,3' );
+// // }
+
+let a = +prompt("a?", "")
+switch(a){
+    case 0:
+        alert(0)
+        break;
+    case 1:
+        alert(1)
+        break;
+    case 2,3:
+        alert("2,3")
+        break;
 }
