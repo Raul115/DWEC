7e67ea6fc2fa05a3c7209bfd2a0e8155132f8229
Raul115 <rulomiguelez@gmail.com>
2021-09-29 09:09:32 +0200

    Login Formatted

--- repos/2Ejercicio/login.js
+++ repos/2Ejercicio/login.js
@@ -1,20 +1,15 @@
 let input = prompt("User name")
-
 if (input == "admin") {
     let pass = prompt("Type the password")
     if (pass == "TheMaster") {
         alert("Welcome!")
-    }
-    else if (pass == null || pass == "") {
+    } else if (pass == null || pass == "") {
         alert("Canceled")
-    }
-    else {
+    } else {
         alert("Wrong password")
     }
-}
-else if (input == null || input == "") {
+} else if (input == null || input == "") {
     alert("Canceled")
-}
-else {
+} else {
     alert("I dont know you")
 }
